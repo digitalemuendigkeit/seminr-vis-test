5adde336ad479b7e237dd6a80ac12c5d04456912
André Calero Valdez <andrecalerovaldez@gmail.com>
2020-12-17 15:24:08 +0100

    updated make file

--- make.R
+++ make.R
@@ -1,4 +1,9 @@
 # This file runs all test documents
 
-rmarkdown::render(input = "template.Rmd", output_file = "output/template.html")
+#update package
+remotes::install_github("digitalemuendigkeit/seminr",
+                        ref = "model-viz", force = TRUE, upgrade = "never")
+
+# run test files
+rmarkdown::render(input = "_template.Rmd", output_file = "output/_template.html")
 rmarkdown::render(input = "simple_case.Rmd", output_file = "output/simple_case.html")
