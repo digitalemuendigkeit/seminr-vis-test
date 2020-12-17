7b43bcd63ed37d852207602219f63e9d16fd63c8
André Calero Valdez <andrecalerovaldez@gmail.com>
2020-12-17 18:08:41 +0100

    added a minimal test for theme-ing.

--- make.R
+++ make.R
@@ -7,3 +7,4 @@ remotes::install_github("digitalemuendigkeit/seminr",
 # run test files
 rmarkdown::render(input = "_template.Rmd", output_file = "output/_template.html")
 rmarkdown::render(input = "simple_case.Rmd", output_file = "output/simple_case.html")
+rmarkdown::render(input = "theme_test.Rmd", output_file = "output/theme_test.html")
